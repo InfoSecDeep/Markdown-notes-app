af05d51d95e6917ba61bb0b214bf301f158e4730
Deep Savaliya <154747338+InfoSecDeep@users.noreply.github.com>
2026-06-30 10:46:07 +0530

    init changes

--- server.js
+++ server.js
@@ -230,7 +230,7 @@ app.get('*', (req, res) => {
 
 // Initialize and start
 initDirs().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}`);
   });
 });
